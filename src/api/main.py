d2cce3debf65d44874b87654bf1a97ac229fe9d3
Philippe Neveux <phil.neveux@gmail.com>
2025-07-24 12:53:49 +1200

    add a new route

--- src/api/main.py
+++ src/api/main.py
@@ -16,3 +16,8 @@ async def health():
 async def gemini(prompt: str):
     response = chat_with_gemini(prompt)
     return {"message": response}
+
+@app.post("/gemini-model")
+async def gemini_model(prompt: str, model_name: str):
+    response = chat_with_gemini(prompt, model_name)
+    return {"message": response}
--- src/chat_my_doc_llms/main.py
+++ src/chat_my_doc_llms/main.py
@@ -3,11 +3,11 @@ import os
 from dotenv import dotenv_values
 from langchain.chat_models import init_chat_model
 
-def chat_with_gemini(message: str):
+def chat_with_gemini(message: str, model_name: str = "gemini-2.0-flash") -> str:
     
 
     model = init_chat_model(
-        "gemini-2.0-flash",
+        model_name,
         model_provider="google_genai"
     )
 
